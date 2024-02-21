97055a2b754441a89d7bf67ac1233892a7649e3e
alessiavozzo <a.vozzo95@gmail.com>
2024-02-21 15:04:26 +0100

    add for cicle js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
-console.log("ok");
+const fizz = "Fizz"
+const buzz = "Buzz"
+const fizzBuzz= "FizzBuzz"
+
+// 1 to 100 in console.log
+
+for (let i = 1; i <= 100; i++){
+    console.log(i);
+
+    let numberChange = i
+
+    if(i % 3 === 0){
+        numberChange = fizz
+        console.log(numberChange);
+    }
+
+    else if(i % 5 === 0){
+        numberChange = buzz
+        console.log(numberChange);
+    }
+
+    else if(i % 3 === 0 && i % 5 === 0){
+        numberChange = fizzBuzz
+        console.log(numberChange);
+    }
+}
