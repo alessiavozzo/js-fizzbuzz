e8c611cbd1f7aa62988e03e307f42f96b105bb56
alessiavozzo <a.vozzo95@gmail.com>
2024-02-21 15:18:16 +0100

    add list items in dom using append

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,34 @@
-const fizz = "Fizz"
-const buzz = "Buzz"
-const fizzBuzz= "FizzBuzz"
+const fizz = "Fizz";
+const buzz = "Buzz";
+const fizzBuzz= "FizzBuzz";
+
+const elementList = document.querySelector("ul")
 
 // 1 to 100 in console.log
 
 for (let i = 1; i <= 100; i++){
-    console.log(i);
+    //console.log(i);
 
-    let numberChange = i
+    let listItem = document.createElement("li")
+    //console.log(listItem);
 
-    if(i % 3 === 0){
-        numberChange = fizz
-        console.log(numberChange);
+    let numberChange = i;
+
+    if(i % 3 === 0 && i % 5 === 0){
+        numberChange = fizzBuzz;
+        //console.log(numberChange);
+    }
+
+    else if(i % 3 === 0){
+        numberChange = fizz;
+        //console.log(numberChange);
     }
 
     else if(i % 5 === 0){
-        numberChange = buzz
-        console.log(numberChange);
+        numberChange = buzz;
+        //console.log(numberChange);
     }
-
-    else if(i % 3 === 0 && i % 5 === 0){
-        numberChange = fizzBuzz
-        console.log(numberChange);
-    }
+    
+    elementList.append(listItem);
+    listItem.append(numberChange)
 }
